fddbf5e4579a293004079f1e7e4362fafae9463f
Erin Green <erin.y.green@gmail.com>
2019-02-20 16:28:40 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,7 @@ class Comment < ActiveRecord::Base
   def user_attributes=(user_attributes)
     user_attributes.values.each do |user_attribute|
       if user_attributes[:username].present?
-        user = User.find_or_create_by(user_attribute)
+        user = User.find_or_create_by(user_attributes)
         if !self.user = user
           self.build_user(:username => user[:username])
         end
