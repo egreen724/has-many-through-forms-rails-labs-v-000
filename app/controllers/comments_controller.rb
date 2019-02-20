f60d30603ea0766de64ec93c37b33002c314cf35
Erin Green <erin.y.green@gmail.com>
2019-02-20 16:30:27 +0000

    Automatically backed up by Learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentsController < ApplicationController
 
   def create
+    if params[:comment][:user_id].blank?
+      params[:comment].delete(:user_id)
+    end
+
     @comment = Comment.create(comment_params)
-    redirect_to post_path(@comment.post)
+    redirect_to @comment.post
   end
 
   private
